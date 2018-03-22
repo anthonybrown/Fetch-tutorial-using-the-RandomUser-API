038c1f9068f0e75dc65ee4a15d5ec3c3547f7178
Tony Brown <tony@tonybrown.me>
2018-03-21 20:32:52 -0400

    update the README file

---
+++ README.md
@@ -0,0 +1,18 @@
+#Woriking with API's
+
+This is another entry for my blog I'm working on.
+I'm creating material for the blog so I have something for the blog,
+makes sense right 🤣 ?
+
+It's fairly simple to work with an API, if you never
+have then it might be a little tricky.
+
+I'll assume you've never used an API before and there are many
+languages you can use to grab a JSON or XML file from an endpoint.
+
+We will be using JavaScript and will be using the JSON object we get
+back from a call to an endpoint that will return our results.
+
+###Todos:
+[ ] Add the HTML for the tutorial
+[ ] Have a launch button to launch the results of the lesson.
---
+++ js/app.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const ul = document.getElementById('authors')
+const url = 'https://randomuser.me/api/?results=30'
+
+function createNode (element) {
+	return document.createElement(element)
+}
+
+function append (parent, el) {
+	return parent.appendChild(el)
+}
+
+fetch(url)
+  .then((res) => res.json())
+	.then(function (data) {
+	  const authors = data.results
+		return authors.map(function (author) {
+		  const li = createNode('li')
+			const img = createNode('img')
+			const span = createNode('span')
+			const email = createNode('email')
+			const cell = createNode('phone');
+			img.src = author.picture.large
+			span.textContent = `${author.name.first} ${author.name.last}`
+			email.textContent = `${author.email}`
+			cell.textContent = `${author.cell}`
+
+			append(li, img)
+			append(li, span)
+			append(li, email)
+			append(li, cell)
+			append(ul, li)
+		})
+	})
+	.catch((err) => {
+	  console.log(JSON.stringify(err));
+	})
+	
+	
+	// 'use strict';
+
+	// function createNode(element) {
+	// 	return document.createElement(element);
+	// }
+	
+	// function append(parent, el) {
+	// return parent.appendChild(el);
+	// }
+	
+	// const ul = document.getElementById('authors');
+	// const url = 'https://randomuser.me/api/?results=10';
+	// fetch(url)
+	// .then((resp) => resp.json())
+	// .then(function(data) {
+	// let authors = data.results;
+	// return authors.map(function(author) {
+	// 	let li = createNode('li'),
+	// 			img = createNode('img'),
+	// 			span = createNode('span');
+	// 	img.src = author.picture.medium;
+	// 	span.innerHTML = `${author.name.first} ${author.name.last}`;
+	// 	append(li, img);
+	// 	append(li, span);
+	// 	append(ul, li);
+	// })
+	// })
+	// .catch(function(error) {
+	// console.log(JSON.stringify(error));
+	// });
