9c01abf0e9748a753cf48d7a816cf8db2c99523e
Tony Brown <tony@tonybrown.me>
2018-04-11 20:12:29 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,34 +8,34 @@ function createNode (element) {
 };
 
 function append (parent, el) {
-	return parent.appendChild(el)
+  return parent.appendChild(el)
 };
 
 fetch(url)
   .then((res) => res.json())
-	.then(function (data) {
-	  const authors = data.results
-		return authors.map(function (author) {
-		  const li = createNode('li')
-			const img = createNode('img')
-			const span = createNode('span')
-			const email = createNode('email')
-			const cell = createNode('phone');
-			img.src = author.picture.large
-			span.textContent = `${author.name.first} ${author.name.last}`
-			email.textContent = `${author.email}`
-			cell.textContent = `${author.cell}`
+  .then(function (data) {
+  const authors = data.results
+    return authors.map(function (author) {
+      const li = createNode('li')
+      const img = createNode('img')
+      const span = createNode('span')
+      const email = createNode('email')
+      const cell = createNode('phone');
+      img.src = author.picture.large
+      span.textContent = `${author.name.first} ${author.name.last}`
+      email.textContent = `${author.email}`
+      cell.textContent = `${author.cell}`
 
-			append(li, img)
-			append(li, span)
-			append(li, email)
-			append(li, cell)
-			append(ul, li)
-		})
-	})
-	.catch((err) => {
-	  console.log(JSON.stringify(err));
-	})
+      append(li, img)
+      append(li, span)
+      append(li, email)
+      append(li, cell)
+      append(ul, li)
+    })
+  })
+  .catch((err) => {
+    console.log(JSON.stringify(err));
+  })
 	
 	
 	// 'use strict';
